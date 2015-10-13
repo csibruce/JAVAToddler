479310ae76d0b9296e1d5566b851309713c20cf7
brucepc <csi.bruce@gmail.com>
2015-10-13 09:40:52 +0900

    JAVAToddler
    java sample code

---
+++ src/zero01_comment_apiGenerator/SampleMain.java
@@ -0,0 +1,9 @@
+package zero01_comment_apiGenerator;
+
+public class SampleMain {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
---
+++ src/zero11_ZacksBJavaAndXmlMapping/test.java
@@ -0,0 +1,13 @@
+package zero11_ZacksBJavaAndXmlMapping;
+
+import java.net.URL;
+
+public class test {
+	public static void main(String[] args) {
+		URL url = ClassLoader.getSystemResource("zero11_ZacksBJavaAndXmlMapping/coninfo.xml");
+		
+		
+		System.out.println("sdafsadf");
+		
+	}
+}
